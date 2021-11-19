53056f6a1a7211897fd73af6ce663523d1c5f722
SW-GAGAN <srgaganreddy0@gmail.com>
2021-11-19 22:54:00 +0530

    Added: Forget Password Ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { LoginComponent } from './components/user/login-component/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgetPasswordComponent } from './components/user/forget-password-component/forget-password.component';
 
 const routes: Routes = [
-  {path:"login", component:LoginComponent}
+  {path:"login", component:LoginComponent},
+  {path:"forget-password", component:ForgetPasswordComponent}
 ];
 
 @NgModule({
